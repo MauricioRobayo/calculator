9f1c5d31ddd7a8c4e50126a57d7c924bacba2588
Mauricio Robayo <rfmajo@gmail.com>
2019-11-28 17:34:10 -0500

    chore(eslint): user babel parser

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,28 @@ import ButtonPanel from './ButtonPanel'
 import calculate from './../logic/calculate' // eslint-disable-line
 import './App.css'
 
-const App = () => (
-  <div className="app">
-    <Display />
-    <ButtonPanel />
-  </div>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    }
+  }
+
+  handleClick = buttonName => {
+    this.setState(state => calculate(buttonName, state))
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Display />
+        <ButtonPanel />
+      </div>
+    )
+  }
+}
 
 export default App
