5c90338d3f690014f7eb243b319094ed3d4419ff
MauricioRobayo <MauricioRobayo@users.noreply.github.com>
2019-12-04 21:12:32 +0000

    Automated deployment: Wed Dec  4 21:12:32 UTC 2019 f1ab095bc0949f9ebd9be9bc69d52707d0030366

---
+++ precache-manifest.89217735b997d7ee5105a260c1a125ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f3b269fc5498675c2c84893b90f7e3",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "e429e8e8ad2730ec0bda",
+    "url": "/calculator/static/css/main.8560294e.chunk.css"
+  },
+  {
+    "revision": "95c4999b17a2703eebb0",
+    "url": "/calculator/static/js/2.1de14429.chunk.js"
+  },
+  {
+    "revision": "e429e8e8ad2730ec0bda",
+    "url": "/calculator/static/js/main.c9d06934.chunk.js"
+  },
+  {
+    "revision": "f69b5c5b50f5b7cd07bd",
+    "url": "/calculator/static/js/runtime-main.736c2c55.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.192aaf173d1cc26a3a17e36e8bab1adc.js"
+  "/calculator/precache-manifest.89217735b997d7ee5105a260c1a125ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
