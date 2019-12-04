6df0772a7aead73d6c1e8fb041f86d0b0734a58b
Mauricio Robayo <rfmajo@gmail.com>
2019-12-04 13:10:57 -0500

    refactor: remove locale string representation
    When converting to a number to use the locale string representation,
    the value when it is '0.0' gets converted to '0', which is not what the
    user just typed.

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Display = ({ result }) => (
-  <div className="display">{Number(result).toLocaleString()}</div>
-)
+const Display = ({ result }) => <div className="display">{result}</div>
 
 Display.propTypes = {
   result: PropTypes.string,
