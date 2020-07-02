65e309c463aea0bcd402b8e758f1d294a895889a
Mauricio Robayo <rfmajo@gmail.com>
2020-07-02 15:25:28 -0500

    style: format with new pretty conf

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,13 +1,13 @@
-import Big from 'big.js/big'
+import Big from 'big.js/big';
 
 const operations = {
   '/': (a, b) => (b.eq(0) ? NaN : a.div(b)),
   '*': (a, b) => a.times(b),
   '-': (a, b) => a.minus(b),
   '+': (a, b) => a.plus(b),
-}
+};
 
 const operate = (num1, num2, operation) =>
-  operations[operation](Big(num1), Big(num2))
+  operations[operation](Big(num1), Big(num2));
 
-export default operate
+export default operate;
